c32dc520a3f47b49a9104dd2e6582df54fe9cd9a
Ryan Nelson <ryannelson6@hotmail.com>
2024-03-25 17:27:11 -0700

    Implemented testing script

--- test.sh
+++ test.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-len="10"
+# Tests the regular expressions in csv style test file
+# First column of csv file is alphabet, second column is length, third column is regex
+# NOTE: alphabet should not be surrounded by [] brackets
 
-while read -r test; do
-    ./dgrep -l$len -r $test > act.test
-    ./dgrep -l$len -r '.*' | /bin/grep -e $test > exp.test
-done < test.txt
+testfile="test.csv"
+separator=','
+
+while IFS="$separator" read -ra testarr; do
+    alpha=${testarr[0]}
+    len=${testarr[1]}
+    regex=${testarr[2]}
+
+    ./dgrep -a ["$alpha"] -l $len -r "$regex" > act.test
+    ./dgrep -a ["$alpha"] -l $len -r '.*' | /bin/grep -e "^$regex$" > exp.test
+
+    diff=$(/bin/diff act.test exp.test)
+    if [ "$diff" != "" ]
+    then
+        echo "Test failed: $regex"
+    fi
+done < "$testfile"
+
+/bin/rm act.test exp.test
