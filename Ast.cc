01c723f98f3377527a60d06a09c37bab93fd5524
Ryan Nelson <ryannelson6@hotmail.com>
2024-04-11 00:43:52 -0700

    Partial implementation of AST module building

--- Ast.cc
+++ Ast.cc
@@ -6,15 +6,24 @@ using std::set;
 using std::vector;
 
 using metastring::Metastring;
+using nfa::Module;
 
 namespace ast {
 
 nfa::Module LiteralNode::buildModule() const {
-    // TODO
+    nfa::Node* start = new nfa::Node();
+    nfa::Node* end = new nfa::Node();
+    start->outgoing.push_back(new nfa::StringEdge(end, lit));
+    return Module(start, end);
 }
 
 nfa::Module CharsetNode::buildModule() const {
-    // TODO
+    nfa::Node* start = new nfa::Node();
+    nfa::Node* end = new nfa::Node();
+    for (char c : chs) {
+        start->outgoing.push_back(new nfa::CharEdge(end, c));
+    }
+    return Module(start, end);
 }
 
 RepeatNode::~RepeatNode() {
@@ -30,11 +39,19 @@ GroupNode::~GroupNode() {
 }
 
 nfa::Module GroupNode::buildModule() const {
-    // TODO
+    nfa::Node* start = new nfa::Node();
+    nfa::Node* end = new nfa::Node();
+    Module inner(child->buildModule());
+    start->outgoing.push_back(new nfa::StartAppendEdge(inner.start, backref));
+    inner.end->outgoing.push_back(new nfa::EndAppendEdge(end, backref));
+    return Module(start, end);
 }
 
 nfa::Module BackrefNode::buildModule() const {
-    // TODO
+    nfa::Node* start = new nfa::Node();
+    nfa::Node* end = new nfa::Node();
+    start->outgoing.push_back(new nfa::BackrefEdge(end, backref));
+    return Module(start, end);
 }
 
 OrNode::~OrNode() {
@@ -43,7 +60,15 @@ OrNode::~OrNode() {
 }
 
 nfa::Module OrNode::buildModule() const {
-    // TODO
+    nfa::Node* start = new nfa::Node();
+    nfa::Node* end = new nfa::Node();
+    Module leftInner(ln->buildModule());
+    start->outgoing.push_back(new nfa::Edge(leftInner.start));
+    leftInner.end->outgoing.push_back(new nfa::Edge(end));
+    Module rightInner(ln->buildModule());
+    start->outgoing.push_back(new nfa::Edge(rightInner.start));
+    rightInner.end->outgoing.push_back(new nfa::Edge(end));
+    return Module(start, end);
 }
 
 ConcatNode::~ConcatNode() {
